94469fd772c42fdefce783153910d7207188939e
Akshit <akshit@MacBookPro-51fb3efe.localdomain>
2024-01-24 14:15:32 +0530

    + https backend

--- src/pages/GNAi/Gnai.tsx
+++ src/pages/GNAi/Gnai.tsx
@@ -1,12 +1,10 @@
 import "./Gnai.css";
 export function Gnai(){
     return(
-        <div className="content p-20">
-            <div className="absolute bottom-0 w-full text-center gptInputBox bg-white">
-                <div className="flex justify-center h-full bg-black">
-                    <div className="flex-grow">
-                        <input type="text" className="w-full h-full border-none outline-0" placeholder="Enter GPT ID" />
-                    </div>
+        <div className="content">
+            <div className="absolute bottom-0 w-full ">
+                <div className="flex justify-center h-full  text-center gptInputBox bg-white mr-10 ml-10">
+                        <input type="text" className="w-full h-full border-none outline-0" placeholder="Send a message...." />
                 </div>
             </div>
 
