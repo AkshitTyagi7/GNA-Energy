9df11fe34bfb16ec0470967590470fdf2d07dffa
Akshit <akshit@MacBookPro-51fb3efe.localdomain>
2024-01-24 21:05:21 +0530

    + gnai

--- src/pages/GNAi/Gnai.tsx
+++ src/pages/GNAi/Gnai.tsx
@@ -1,13 +1,59 @@
 import "./Gnai.css";
+import {ReactComponent as UserIcon} from '../../icons/user.svg';
 export function Gnai(){
     return(
         <div className="content">
+            <div className="chatArea pt-10">
+                <ChatBox sender="User" message="Hello" />
+            </div>
+
+            <div className="chatArea pt-10">
+                <ChatBox sender="User" message="Hi" />
+            </div>
+
+
+            <div className="chatArea pt-10">
+                <ChatBox sender="User" message="Hello" />
+            </div>
+
+            <div className="chatArea pt-10">
+                <ChatBox sender="User" message="Hello" />
+            </div>
+
+            <div className="chatArea pt-10">
+                <ChatBox sender="User" message="Hello" />
+            </div>
+
             <div className="absolute bottom-0 w-full ">
-                <div className="flex justify-center h-full  text-center gptInputBox bg-white mr-10 ml-10">
+
+                <div className="flex justify-center h-full  text-center gptInputBox bg-white">
                         <input type="text" className="w-full h-full border-none outline-0" placeholder="Send a message...." />
                 </div>
             </div>
 
+        </div>
+    )
+
+
+}
+
+function ChatBox({
+    sender,
+    message
+}:{
+    sender: string,
+    message: string
+}): JSX.Element{
+    return(
+        <div className="messageBox">
+            <div className="flex">
+                <UserIcon className="w-8 h-8" />
+               <div>
+                <div className="messageSender">{sender}</div>
+                <div className="message">{message}</div>
+
+               </div>
+            </div>
         </div>
     )
 }
