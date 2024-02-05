2f1ff30be2a9e362e1bbaa24905a43d5a034f2aa
Akshit Tyagi <AkshitTyagi7>
2024-02-05 15:45:37 +0530

    +enai https

--- src/pages/GNAi/Gnai.tsx
+++ src/pages/GNAi/Gnai.tsx
@@ -82,7 +82,7 @@ export function Gnai() {
 
         scrollToBottom();
         await new Promise(r => setTimeout(r, 100));
-        const response = await fetch("https://ec2-13-235-113-18.ap-south-1.compute.amazonaws.com/api/streamChatGpt", {
+        const response = await fetch("https://assistant.gna.energy/api/streamChatGpt", {
             method: "post",
             headers: {
               Accept: "application/json, text/plain, */*",
